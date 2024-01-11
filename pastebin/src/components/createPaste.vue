28ba6e06274ecb24fa1605c4eb98ef9f254095a2
ZincCat <zincchloride@outlook.com>
2024-01-11 15:47:24 -0500

    createPaste skeleton

---
+++ pastebin/src/components/createPaste.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Create Paste</h1>
+    <form @submit="createPaste">
+      <textarea v-model="pasteContent" rows="5"></textarea>
+      <button type="submit">Create Paste</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      pasteContent: '',
+    };
+  },
+  methods: {
+    createPaste() {
+      axios
+        .post('/api/pastes', { content: this.pasteContent })
+        .then(response => {
+          // Handle success
+          console.log(response.data);
+        })
+        .catch(error => {
+          // Handle error
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
+
+<style>
+/* Add your CSS styles here */
+</style>
