92098cad4c69cdb7186aa9a351826c10665c5635
ZincCat <zincchloride@outlook.com>
2024-01-11 16:03:27 -0500

    changed router

--- pastebin/src/router/index.js
+++ pastebin/src/router/index.js
@@ -1,20 +1,42 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../components/Home.vue'
 import Login from '../components/Login.vue'
 import Register from '../components/Register.vue'
 import AddPaste from '../components/AddPaste.vue'
 import ViewPaste from '../components/ViewPaste.vue'
 
-Vue.use(Router)
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/add',
+      name: 'AddPaste',
+      component: AddPaste
+    },
+    {
+      path: '/view/:id',
+      name: 'ViewPaste',
+      component: ViewPaste
+    }
+]
 
-export default new Router({
-  mode: 'history',
-  routes: [
-    { path: '/', component: Home },
-    { path: '/login', component: Login },
-    { path: '/register', component: Register },
-    { path: '/add', component: AddPaste },
-    { path: '/view/:id', component: ViewPaste }
-  ]
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
 })
+
+export default router
