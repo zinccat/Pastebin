dafac1f029b099d208dc61dbaf493ea6daa267f2
ZincCat <zincchloride@outlook.com>
2024-01-11 19:08:35 -0500

    fix secret

--- pastebin_backend/app.py
+++ pastebin_backend/app.py
@@ -3,14 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pastebin.db'
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'  # Add this line
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 
 CORS(app)
 db = SQLAlchemy(app)
